105d76bbbb833e76b9804ea58172c821cdbf31e0
Nikita Kutselev <nikita@akuity.io>
2022-12-14 16:01:06 -0800

    Squashed commit of the following:
    commit 53f3972d6bd7170f33a8d6066dcb9bc18a7e169d
    Author: Nikita Kutselev <nikita@akuity.io>
    Date:   Wed Dec 14 15:50:21 2022 -0800
    
        Add akp instance
    
    commit 01589c33ed6eaca9a589da660c3f99e9f6997303
    Author: Nikita Kutselev <nikita@akuity.io>
    Date:   Wed Dec 14 15:25:37 2022 -0800
    
        Add gke and vpc

--- akuity-gcp/gke.tf
+++ akuity-gcp/gke.tf
@@ -1,19 +1,55 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
+  project     = var.google_project_id
+  region      = var.google_region
 }
 
 module "vpc" {
   source       = "terraform-google-modules/network/google"
-  version      = "5.1.0"
-  project_id   = var.project_id
-  network_name = "test-vpc"
+  version      = "~> 6.0"
+  project_id   = var.google_project_id
+  network_name = "example-vpc"
 
   subnets = [
     {
-      subnet_name   = "subnet-01"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = var.region
+      subnet_name   = "example-subnet"
+      subnet_ip     = cidrsubnet(var.google_cidr_base, 4, 1)
+      subnet_region = var.google_region
     }
   ]
+  secondary_ranges = {
+    example-subnet = [
+      {
+        range_name = "example-gke-pods"
+        ip_cidr_range = "192.168.64.0/20"
+      },
+      {
+        range_name = "example-gke-services"
+        ip_cidr_range = "192.168.80.0/24"
+      }
+    ]
+  }
+}
+
+data "google_client_config" "default" {}
+
+provider "kubectl" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.google_project_id
+  name                       = "gke-test-1"
+  region                     = var.google_region
+  zones                      = var.google_zones
+  network                    = module.vpc.network_name
+  subnetwork                 = module.vpc.subnets_names[0]
+  ip_range_pods              = "example-gke-pods"
+  ip_range_services          = "example-gke-services"
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
 }
