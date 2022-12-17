d132a32e1c92a120bfcb5f0863b8314803cbbe18
Nikita Kutselev <nikita@akuity.io>
2022-12-16 20:03:16 -0800

    Add karpenter to eks

--- 100-clusters/cluster-eks.tf
+++ 100-clusters/cluster-eks.tf
@@ -13,6 +13,7 @@ data "aws_partition" "current" {}
 
 module "aws_vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
 
   name = "example-network"
   cidr = var.aws_cidr_base
@@ -25,12 +26,15 @@ module "aws_vpc" {
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
   enable_vpn_gateway     = false
-  
+
   public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
   }
 
   private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    "karpenter.sh/discovery" = "true"
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
   }
 }
@@ -46,13 +50,16 @@ module "eks" {
   cluster_endpoint_public_access  = true
 
   enable_irsa = true
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = local.eks_cluster_name
+  }
   eks_managed_node_groups = {
-    cpu-high = {
-      instance_types = ["c5.xlarge"]
+    initial = {
+      instance_types        = ["m5.large"]
       create_security_group = false
-      min_size     = 2
-      max_size     = 6
-      desired_size = 3
+      min_size              = 2
+      max_size              = 3
+      desired_size          = 2
     }
   }
   cluster_addons = {
@@ -82,4 +89,107 @@ module "vpc_cni_irsa" {
       namespace_service_accounts = ["kube-system:aws-node"]
     }
   }
+}
+
+#-----------Karpenter----------
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "18.31.0"
+
+  cluster_name = module.eks.cluster_name
+
+  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+
+  create_iam_role = false
+  iam_role_arn    = module.eks.eks_managed_node_groups["initial"].iam_role_arn
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.default.token
+  }
+}
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  version    = "v0.20.0"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter.irsa_arn
+  }
+
+  set {
+    name  = "settings.aws.clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "settings.aws.clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = module.karpenter.instance_profile_name
+  }
+
+  set {
+    name  = "settings.aws.interruptionQueueName"
+    value = module.karpenter.queue_name
+  }
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  provider  = kubectl.eks_1
+  yaml_body = <<-YAML
+  apiVersion: karpenter.sh/v1alpha5
+  kind: Provisioner
+  metadata:
+    name: default
+  spec:
+    requirements:
+      - key: karpenter.sh/capacity-type
+        operator: In
+        values: ["spot"]
+    limits:
+      resources:
+        cpu: 1000
+    providerRef:
+        name: default
+    ttlSecondsAfterEmpty: 30
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+resource "kubectl_manifest" "karpenter_node_template" {
+  provider  = kubectl.eks_1
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1alpha1
+    kind: AWSNodeTemplate
+    metadata:
+      name: default
+    spec:
+      subnetSelector:
+        karpenter.sh/discovery: "true"
+      securityGroupSelector:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
 }
