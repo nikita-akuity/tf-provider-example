c46be21f5656b6f5b9a04b432876f3fe0256246a
Nikita Kutselev <nikita@akuity.io>
2022-12-21 11:06:25 -0800

    Addl one cluster

--- 1-cluster/tf-files/akp.tf
+++ 1-cluster/tf-files/akp.tf
@@ -15,3 +15,47 @@ provider akp {
 data "akp_instance" "argocd" {
   name    = "tf-managed-demo"
 }
+
+resource "akp_cluster" "cluster" {
+  instance_id      = data.akp_instance.argocd.id
+  name             = "one"
+  namespace        = "akuity"
+  size             = "small"
+  labels           = {
+    # example = "1-cluster"
+    cloud   = "azure"
+    dev     = "true"
+    stage   = "true"
+    prod    = "true"
+  }
+}
+
+data "kubectl_file_documents" "agent" {
+    content = akp_cluster.cluster.manifests
+}
+
+resource "kubectl_manifest" "agent_namespace" {
+    yaml_body = element(data.kubectl_file_documents.agent.documents, 0)
+    wait      = true
+    lifecycle {
+      ignore_changes = [
+        yaml_body
+      ]
+    }
+}
+
+resource "kubectl_manifest" "agent" {
+    count     = 30
+    yaml_body = element(data.kubectl_file_documents.agent.documents, count.index + 1)
+    # Important!
+    wait_for_rollout = false
+    depends_on = [
+        kubectl_manifest.agent_namespace
+    ]
+    # Agent manages itself, no need to reapply manifests
+    lifecycle {
+      ignore_changes = [
+        yaml_body
+      ]
+    }
+}
