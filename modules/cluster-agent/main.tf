0fe36f14069cae9517df1eef5a9dedcb16c3a025
Nikita Kutselev <nikita@akuity.io>
2022-12-21 10:38:42 -0800

    Add lifecycle to cluster-agent module

--- modules/cluster-agent/main.tf
+++ modules/cluster-agent/main.tf
@@ -13,20 +13,31 @@ resource "akp_cluster" "cluster" {
 }
 
 data "kubectl_file_documents" "agent" {
-    content = akp_cluster.cluster.manifests
+  content = akp_cluster.cluster.manifests
 }
 
 resource "kubectl_manifest" "agent_namespace" {
-    yaml_body = element(data.kubectl_file_documents.agent.documents, 0)
-    wait      = true
+  yaml_body = element(data.kubectl_file_documents.agent.documents, 0)
+  wait      = true
+  lifecycle {
+    ignore_changes = [
+      yaml_body
+    ]
+  }
 }
 
 resource "kubectl_manifest" "agent" {
-    count     = local.manifest_count - 1
-    yaml_body = element(data.kubectl_file_documents.agent.documents, count.index + 1)
-    # Important!
-    wait_for_rollout = false
-    depends_on = [
-        kubectl_manifest.agent_namespace
+  count     = local.manifest_count - 1
+  yaml_body = element(data.kubectl_file_documents.agent.documents, count.index + 1)
+  # Important!
+  wait_for_rollout = false
+  depends_on = [
+      kubectl_manifest.agent_namespace
+  ]
+  # Agent manages itself, no need to reapply manifests
+  lifecycle {
+    ignore_changes = [
+      yaml_body
     ]
+  }
 }
